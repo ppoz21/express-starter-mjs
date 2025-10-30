814c81af59c317520b4759fbfb2dfb456b154436
Paweł Palacz <pawel.palacz@in2dev.pl>
2025-10-30 10:04:53 +0100

    fix

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import express from 'express'
 const app = express()
 
 app.listen(3000, () => {
-  console.log('App is running on https://localhost:3000')
+  console.log('App is running on http://localhost:3000')
 })
